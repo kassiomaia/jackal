33a0240fc965d323a22f8450a20b6de94d585707
Kássio Maia <kassio.maia@gmail.com>
2023-01-12 21:06:00 -0300

    start IR representation module

--- lib/jackal_eval.c
+++ lib/jackal_eval.c
@@ -8,7 +8,6 @@ jkl_word_t jkl_eval(jkl_program_t *program)
 {
   invariant(program == NULL, "program is NULL");
   invariant(program->ast_prog_root == NULL, "ast root is NULL");
-  invariant(program->error != 0, "program has error");
   invariant(program->ast_prog_root->type != JKL_NODE_BLOCK, "ast root is not block");
 
   jkl_log("jkl_eval", "evaluating program");
