c3a47516cf9812c37b9e6b700716566d791e331a
Kássio Maia <kassio.maia@gmail.com>
2023-01-13 01:22:01 -0300

    add stack and hash object support

---
+++ include/jackal/jackal_hash.h
@@ -0,0 +1,35 @@
+#ifndef JACKAL_HASHOBJ_H
+#define JACKAL_HASHOBJ_H
+
+#include <jackal.h>
+
+typedef enum {
+  JKL_HASH_TYPE_STRING,
+  JKL_HASH_TYPE_INT,
+  JKL_HASH_TYPE_FLOAT,
+  JKL_HASH_TYPE_BOOL,
+  JKL_HASH_TYPE_NULL,
+  JKL_HASH_TYPE_ARRAY,
+  JKL_HASH_TYPE_OBJECT,
+  JKL_HASH_TYPE_NIL
+} jkl_hash_type_t;
+
+typedef struct {
+  char *key;
+  void *data;
+  jkl_hash_type_t type;
+} jkl_hash_pair;
+
+typedef struct {
+  jkl_hash_pair *keys;
+  unsigned int size;
+  unsigned int capacity;
+} jkl_hash_tbl;
+
+void jkl_hash_init(jkl_hash_tbl *tbl);
+void jkl_hash_free(jkl_hash_tbl *tbl);
+void jkl_hash_set(jkl_hash_tbl *tbl, jkl_string_t key, jkl_any_t data, jkl_word_t type);
+void *jkl_hash_get(jkl_hash_tbl *tbl, jkl_string_t key);
+void jkl_hash_del(jkl_hash_tbl *tbl, jkl_string_t key);
+
+#endif
---
+++ lib/jackal_stack.c
@@ -0,0 +1,48 @@
+#include <jackal.h>
+
+void stack_init(stack_t *stack, unsigned int capacity)
+{
+  stack->capacity = capacity;
+  stack->bp = 0;
+  stack->sp = 0;
+  stack->items = (int *)malloc(sizeof(stack_item_t) * capacity);
+
+  if (stack->items == NULL)
+    jkl_error("jkl_stack", "stack_init", "malloc failed");
+
+  memset(stack->items, 0, sizeof(stack_item_t) * capacity);
+}
+
+void stack_push(stack_t *stack, stack_item_t item)
+{
+  if (stack->sp == stack->capacity)
+    jkl_error("jkl_stack", "stack_push", "stack overflow");
+
+  stack->items[stack->sp++] = item;
+}
+
+stack_item_t stack_pop(stack_t *stack)
+{
+  if (stack->sp == stack->bp)
+    jkl_error("jkl_stack", "stack_pop", "stack underflow");
+
+  return stack->items[--stack->sp];
+}
+
+stack_item_t stack_peek(stack_t *stack)
+{
+  if (stack->sp == stack->bp)
+    jkl_error("jkl_stack", "stack_peek", "stack underflow");
+
+  return stack->items[stack->sp - 1];
+}
+
+unsigned int stack_size(stack_t *stack)
+{
+  return stack->sp - stack->bp;
+}
+
+void stack_free(stack_t *stack)
+{
+  free(stack->items);
+}
